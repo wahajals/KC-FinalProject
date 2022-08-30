bddefdfe61b36f78463f2fe27d6f0408b3da370e
Wahaj <100323687+wahajals@users.noreply.github.com>
2022-08-30 20:39:48 +0300

    added project

---
+++ iCare/iCare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCare
+//
+//  Created by WSS on 29/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("iCare")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCare/iCare/iCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCareApp.swift
+//  iCare
+//
+//  Created by WSS on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct iCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
